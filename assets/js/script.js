0dee63759a50981b2853f041b2f04a56ac7050c3
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-05-22 16:16:48 -0400

    added the rest of the time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,7 @@ function updateTime() {
     var timeBlock = parseInt($(this).attr("id").split("-")[1]);
 
     console.log("block hour:", timeBlock);
-    // check if we've moved past this time
-    // if the current hour is greater than the block hour
-    // then add class "past"
-    // if they are equal
-    // then remove class "past" and add class "present"
-    // else
-    // remove class "past", remove class "present", add class "future"
+
     if (timeBlock < currentTime) {
       $(this).addClass("past");
     } else if (timeBlock === currentTime) {
@@ -32,9 +26,6 @@ $(document).ready(function () {
     // get nearby values
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-
-    console.log("value:", value);
-    console.log("time:", time);
 
     // save the value in localStorage as time
     localStorage.setItem(time, value);
